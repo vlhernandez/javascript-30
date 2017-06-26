603aaf1151c15f5a398430c4812f9437cbc1e9f1
Veronica Hernandez <vlhernandez@gmail.com>
2017-06-26 15:54:18 -0500

    just followed along this time

--- 19/scripts.js
+++ 19/scripts.js
@@ -3,3 +3,97 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch( err => {
+      console.err(`OH NO!!!`, err);
+    })
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // grab pixels from canvas
+    let pixels = ctx.getImageData(0, 0, width, height);
+
+    // alter the pixels
+    // pixels = redEffect(pixels);
+    // pixels = rgbSplit(pixels);
+    pixels = greenScreen(pixels);
+    // put altered pixels back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'photoBooth');
+  link.innerHTML = `<img src="${data}">`;
+  strip.insertBefore(link, strip.firstChild)
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100;  //red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //green
+    pixels.data[i + 2] = pixels.data[i + 2] * .5; //blue
+  }
+ return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0];  //red
+    pixels.data[i + 100] = pixels.data[i + 1]; //green
+    pixels.data[i - 150] = pixels.data[i + 2]; //blue
+  }
+ return pixels;
+}
+
+function greenScreen(pixels) {
+  let levels = {};
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (let i = 0; i < pixels.data.length; i+=4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    // 4th pixel is alpha, if set to zero it is transparent
+    if( red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax ) {
+        pixels.data[i + 3] = 0;
+      }
+  }
+
+  return pixels;
+}
+
+getVideo();
+
+video.addEventListener( 'canplay', paintToCanvas );

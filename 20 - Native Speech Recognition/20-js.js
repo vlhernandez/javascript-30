1715d16ab0a9b999d80ce3798f96bf1f6b1b8dde
Veronica Hernandez <vlhernandez@gmail.com>
2017-06-27 11:17:00 -0500

    capturing speech

--- 20 - Native Speech Recognition/20-js.js
+++ 20 - Native Speech Recognition/20-js.js
@@ -2,3 +2,21 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
 
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
+
+let p = document.createElement('p');
+
+const transcript = document.querySelector('.transcript');
+transcript.appendChild(p);
+
+recognition.addEventListener('result', e => {
+  const words = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join(" ");
+
+  console.log(words);
+})
+
+recognition.addEventListener('end', recognition.start);
+
+recognition.start();
